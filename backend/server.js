86cf36538c5911c4058d3ce5fbd0de1f54d4405b
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-08-02 21:08:46 +0500

    Created middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
 app.get('/', (req, res) => {
     res.json({msg: 'Welcome to Training Bro!'});
 });
