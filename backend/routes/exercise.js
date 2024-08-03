b15ef2b68dcddcbf0498639102e42888b87c593d
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-08-03 14:25:03 +0500

    Creating all routes(dummy) to handle workouts

--- backend/routes/exercise.js
+++ backend/routes/exercise.js
@@ -1,2 +1,24 @@
 const express = require('express');
 const expressRouter = express.Router();
+
+expressRouter.get('/', (req, res) => {
+    res.json({msg:'GET all Exercises'});
+});
+
+expressRouter.get('/:id', (req, res) => {
+    res.json({msg:'GET a single Exercise'});
+});
+
+expressRouter.post('/', (req, res) => {
+    res.json({msg:'POST Exercise'});
+});
+
+expressRouter.delete('/:id', (req, res) => {
+    res.json({msg:'DELETE Exercise'});
+});
+
+expressRouter.patch('/:id', (req, res) => {
+    res.json({msg:'UPDATE Exercise'});
+});
+
+module.exports = expressRouter;
--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const exerciseRoutes = require('./routes/exercise');
+
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 });
 
-app.get('/', (req, res) => {
-    res.json({msg: 'Welcome to Training Bro!'});
-});
+app.use('/api/exercises', exerciseRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Port no: ${process.env.PORT}`);
