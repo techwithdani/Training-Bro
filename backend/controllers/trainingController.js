46745ae936fac99fcc7e9792e4f0dfe2d0053ed7
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-08-04 14:36:20 +0500

    Checking if the Id is valid as per mongoose and mongodb to avoid crashing app

--- backend/controllers/trainingController.js
+++ backend/controllers/trainingController.js
@@ -1,4 +1,5 @@
 const trainingModel = require('../models/trainingModel');
+const mongoose = require('mongoose');
 
 const postExercise = async (req, res) => {
     const {title, reps, load} = req.body;
@@ -20,6 +21,10 @@ const getAllExercises = async (req, res) => {
 const getSingleExercise = async (req, res) => {
     const {id} = req.params;
     const exercise = await trainingModel.findById(id);
+
+    if (!mongoose.Types.ObjectId.isValid()) {
+        return res.status(404).json({error: "Invalid Id and Exercise doesn't exist."});
+    }
 
     if (!exercise) {
         return res.status(404).json({error: "Exercise doesn't exist."});
