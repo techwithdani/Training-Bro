c3eb85ce2f0d44d109f56be3dde0d39cfccb4a55
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-08-06 15:17:34 +0500

    Displaying fetched data on the browser

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -19,9 +19,19 @@ const Home = () => {
     return (
         <div className="home">
             <div className="exercises">
-                {exercises && exercises.map((exercise) => (
-                    <p key={exercise._id}>{exercise}</p>
-                ))}
+                {exercises ? (
+                    exercises.map((exercise) => (
+                        <div key={exercise._id}>
+                            <h3>{exercise.title}</h3>
+                            <p>Reps: {exercise.reps}</p>
+                            <p>Load: {exercise.load}Kg</p>
+                            <p>Created At: {new Date(exercise.createdAt).toLocaleString()}</p>
+                            <p>Updated At: {new Date(exercise.updatedAt).toLocaleString()}</p>
+                        </div>
+                    ))
+                ) : (
+                    <p>Loading exercises...</p>
+                )}
             </div>
         </div>
     )
