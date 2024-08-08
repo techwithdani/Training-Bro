92b21066edbee3840f463dabf513864d72e3fdc2
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-08-08 16:12:27 +0500

    Adding useReducer for context provider to function properly

--- frontend/src/context/TrainingContext.jsx
+++ frontend/src/context/TrainingContext.jsx
@@ -1,11 +1,35 @@
-import { createContext } from "react"
+import PropTypes from 'prop-types';
+import { createContext, useReducer } from "react"
 
 export const TrainingContext = createContext()
 
-export const TrainingContextProvider = () => {
+export const trainingReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_EXERCISES':
+            return {
+                exercises: action.payload
+            }
+        case 'CREATE_EXERCISE':
+            return {
+                exercises: [action.payload, ...state.exercises]
+            }
+        default:
+            return state
+    }
+}
+
+export const TrainingContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(trainingReducer, {
+        exercises: null
+    })
+
     return (
-        <TrainingContext.Provider>
-            
+        <TrainingContext.Provider value={{state, dispatch}}>
+            { children }
         </TrainingContext.Provider>
     )
 }
+
+TrainingContextProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
